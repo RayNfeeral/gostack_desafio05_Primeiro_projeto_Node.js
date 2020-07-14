7945bafaab36cc95d005b978169f5930bc0bf460
Rafael Nery Machado <rafalenerymachado@gmail.com>
2020-07-14 14:31:52 -0300

    Create and list funcionality implemented

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,35 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((incomeTotal, transaction) => {
+      if (transaction.type !== 'income') return incomeTotal;
+
+      return incomeTotal + transaction.value;
+    }, 0);
+    const outcome = this.transactions.reduce((outcomeTotal, transaction) => {
+      if (transaction.type !== 'outcome') return outcomeTotal;
+
+      return outcomeTotal + transaction.value;
+    }, 0);
+    const total = income - outcome;
+
+    return {
+      income,
+      outcome,
+      total,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
